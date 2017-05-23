ee4d91df314433245a8275442ae30deb5c7f9e8b
Bhavika Tekwani <bhavicka.7992@gmail.com>
2017-05-23 14:58:11 -0400

    Updated paths, layer name mismatch solution in docs

--- src/gmu_script.py
+++ src/gmu_script.py
@@ -1,6 +1,9 @@
+from pip.cmdoptions import allow_external
+
 from geoserver_utils.Catalog import CatalogUtils
 import os
 from os.path import basename
+from geoserver.catalog import Catalog
 
 alexandria_2007_path = "/media/bhavika/Bhavika/BHAVIKA/Geodata/all/Alexandria_2007_DataCD"
 arlington_2004_path = "/media/bhavika/Bhavika/BHAVIKA/Geodata/all/Arlington_2004"
@@ -19,14 +22,18 @@ def find_shapefiles(path):
                 name = os.path.splitext(x)[0]
                 shpfiles[name] = os.path.join(dirpath, x)
     for shp in shpfiles.keys():
-         file_path = shpfiles[shp]
-         print file_path
-         c.add_shapefile(data=file_path, ws="GMUGeodata")
+        file_path = shpfiles[shp]
+        print file_path
+        c.add_shapefile(data=file_path, ws="GMUGeodata")
     print shpfiles
 
 
+# def edit_layers(workspace):
+#     cat = Catalog("http://localhost:8080/geoserver/rest", username="admin", password="bhavika1992")
+#     r = cat.get_resources(store="GMUPrivate_Open_Spaces", workspace=workspace)
+#     layers = cat.get_layers(r)
+
 find_shapefiles(alexandria_2007_path)
-
-
+# edit_layers("GMUGeodata")
 
 # print c.store_exists("GMUBLDG")
